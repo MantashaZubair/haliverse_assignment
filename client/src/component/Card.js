6fe0e4686fb5e2fbf8be32b28cea381dc7954911
Mantasha Zubair <98265444+MantashaZubair@users.noreply.github.com>
2023-07-10 02:14:32 +0530

    first-commit

--- client/src/component/Card.js
+++ client/src/component/Card.js
@@ -7,8 +7,9 @@ const Card = ({item}) => {
   <div className="col-sm-12 col-lg-4 col-md-6">
     <div className="card mt-3">
     <div className="card-body">
-              <h5 className="card-title fw-bold">Name:{item.first_name}</h5>
-              <p className="card-text">Domain:{item.domain}</p>
+              <h5 className="card-title fw-bold">Name:{item?.first_name}</h5>
+              <p className="card-text">Gender:{item?.gender}</p> 
+              <p className="card-text">Domain:{item?.domain}</p>        
               <p className="card-text fw-bold">Avalable:{item.available===true ? <><span className='text-success'>yes available</span></> : <><span className='text-warning'>  not available</span></>}</p>
               <button className="btn btn-primary ms-2 " onClick={()=>navigate(`/userDetails/${item._id}`)}>More Details</button>
               <button className="btn btn-success ms-3">
